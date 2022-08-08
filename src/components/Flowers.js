7608f7d47f2efca0d5c04c7e5dca34bbc2f0c5b2
JovanMilutinovicGit <milutinovicjovan248@gmail.com>
2022-08-08 23:40:55 +0200

    init

--- src/components/Flowers.js
+++ src/components/Flowers.js
@@ -1,8 +1,23 @@
 import React from 'react'
-import NewAccount from './NewAccount.js'
+import FlowersList from './FlowersList.js'
+import "./Flowers.css"
 
 const Flowers = () => {
-    return <div></div>
+    return(
+    <div>
+    <div className='flowers'>
+    <div className="discoverSearch">
+            <div className="form-inputSearch">
+            <span className="iconSearch"><i className="fa fa-search"></i></span>
+            <input
+            placeholder="Looking for something specific?"
+            />
+            </div>
+           </div> 
+           </div>
+    <FlowersList/>
+    </div>
+    );
 }
 
 export default Flowers;
